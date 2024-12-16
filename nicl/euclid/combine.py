36bb052d0aac85645232b862d047c58c6b45bac7
Hua Gao <hgao.astro@gmail.com>
2024-12-16 16:31:40 +0000

    close #55

--- nicl/euclid/combine.py
+++ nicl/euclid/combine.py
@@ -19,122 +19,9 @@ from photutils.background import Background2D
 
 from nicl.euclid.data_access import DataAccess
 from nicl.mask import fast_mask
+from nicl.euclid.constants import VIS, NISP, SWARP_CONFIG_NISP, SWARP_CONFIG_VIS
 
 # %% ../../nbs/euclid/combine.ipynb 4
-swarp_config_nisp = """
-#----------------------------------- Output -----------------------------------
-IMAGEOUT_NAME          coadd.fits      # Output filename
-WEIGHTOUT_NAME         coadd.weight.fits # Output weight-map filename
- 
-HEADER_ONLY            N               # Only a header as an output file (Y/N)?
-HEADER_SUFFIX          .head           # Filename extension for additional headers
- 
-#------------------------------- Input Weights --------------------------------
-
-WEIGHT_TYPE            MAP_WEIGHT    # BACKGROUND,MAP_RMS,MAP_VARIANCE
-                                       # or MAP_WEIGHT
-RESCALE_WEIGHTS        N               # Rescale input weights/variances (Y/N)?
-WEIGHT_SUFFIX          .weight.fits    # Suffix to use for weight-maps
-WEIGHT_IMAGE                           # Weightmap filename if suffix not used
-                                       # (all or for each weight-map)
-WEIGHT_THRESH                          # Bad pixel weight-threshold
- 
-#------------------------------- Co-addition ----------------------------------
- 
-COMBINE                Y               # Combine resampled images (Y/N)?
-COMBINE_TYPE           WEIGHTED         # MEDIAN,AVERAGE,MIN,MAX,WEIGHTED,CLIPPED
-                                       # CHI-OLD,CHI-MODE,CHI-MEAN,SUM,
-                                       # WEIGHTED_WEIGHT,MEDIAN_WEIGHT,
-                                       # AND,NAND,OR or NOR
-#CLIP_AMPFRAC           0.3             # Fraction of flux variation allowed
-                                       # with clipping
-#CLIP_SIGMA             4.0             # RMS error multiple variation allowed
-                                       # with clipping
-#CLIP_WRITELOG          N               # Write output file with coordinates of
-                                       # clipped pixels (Y/N)
-#CLIP_LOGNAME           clipped.log     # Name of output file with coordinates
-                                       # of clipped pixels
-#BLANK_BADPIXELS        N               # Set to 0 pixels having a weight of 0
- 
-#-------------------------------- Astrometry ----------------------------------
- 
-CELESTIAL_TYPE         NATIVE          # NATIVE, PIXEL, EQUATORIAL,
-                                       # GALACTIC,ECLIPTIC, or SUPERGALACTIC
-PROJECTION_TYPE        TAN             # Any WCS projection code or NONE
-PROJECTION_ERR         0.001           # Maximum projection error (in output
-                                       # pixels), or 0 for no approximation
-CENTER_TYPE            ALL             # MANUAL, ALL or MOST
-CENTER         00:00:00.0, +00:00:00.0 # Coordinates of the image center
-PIXELSCALE_TYPE        MANUAL          # MANUAL,FIT,MIN,MAX or MEDIAN
-PIXEL_SCALE            0.3             # Pixel scale
-IMAGE_SIZE             0               # Image size (0 = AUTOMATIC)
- 
-#-------------------------------- Resampling ----------------------------------
- 
-RESAMPLE               Y               # Resample input images (Y/N)?
-RESAMPLE_DIR           .               # Directory path for resampled images
-RESAMPLE_SUFFIX        .resamp.fits    # filename extension for resampled images
- 
-RESAMPLING_TYPE        LANCZOS2        # NEAREST,BILINEAR,LANCZOS2,LANCZOS3
-                                       # LANCZOS4 (1 per axis) or FLAGS
-OVERSAMPLING           0               # Oversampling in each dimension
-                                       # (0 = automatic)
-INTERPOLATE            N               # Interpolate bad input pixels (Y/N)?
-                                       # (all or for each image)
- 
-FSCALASTRO_TYPE        VARIABLE            # NONE,FIXED, or VARIABLE
-FSCALE_KEYWORD         PHOSCALE            # FITS keyword for the multiplicative
-                                       # factor applied to each input image
-FSCALE_DEFAULT         1.0             # Default FSCALE value if not in header
- 
-GAIN_KEYWORD           GAIN            # FITS keyword for effect. gain (e-/ADU)
-GAIN_DEFAULT           0.0             # Default gain if no FITS keyword found
-
-SATLEV_KEYWORD         SATURATE        # FITS keyword for saturation level (ADU)
-SATLEV_DEFAULT         50000.0         # Default saturation if no FITS keyword
-
-#--------------------------- Background subtraction ---------------------------
- 
-SUBTRACT_BACK          N               # Subtraction sky background (Y/N)?
-                                       # (all or for each image)
- 
-BACK_TYPE              AUTO            # AUTO or MANUAL
-                                       # (all or for each ima  ge)
-BACK_DEFAULT           0.0             # Default background value in MANUAL
-                                       # (all or for each image)
-BACK_SIZE              32             # Background mesh size (pixels)
-                                       # (all or for each image)
-BACK_FILTERSIZE        5               # Background map filter range (meshes)
-                                       # (all or for each image)
- 
-#------------------------------ Memory management -----------------------------
- 
-VMEM_DIR               .               # Directory path for swap files
-VMEM_MAX               6144            # Maximum amount of virtual memory (MB)
-MEM_MAX                6144            # Maximum amount of usable RAM (MB)
-COMBINE_BUFSIZE        6144            # RAM dedicated to co-addition(MB)
- 
-#------------------------------ Miscellaneous ---------------------------------
- 
-DELETE_TMPFILES        Y               # Delete temporary resampled FITS files
-                                       # (Y/N)?
-COPY_KEYWORDS          RA, DEC, DATAUNIT, DET_ID, ZPAB, ZPABE, ZPVEGA, ZPVEGAE, OBJECT # List of FITS keywords to propagate
-                                       # from the input to the output headers
-WRITE_FILEINFO         N               # Write information about each input
-                                       # file in the output image header?
-WRITE_XML              Y               # Write XML file (Y/N)?
-XML_NAME               swarp.xml       # Filename for XML output
-VERBOSE_TYPE           NORMAL          # QUIET,LOG,NORMAL, or FULL
- 
-NTHREADS               0               # Number of simultaneous threads for
-                                       # the SMP version of SWarp
-                                       # 0 = automatic
-"""
-
-# The VIS config may need some customisations
-swarp_config_vis = swarp_config_nisp
-
-# %% ../../nbs/euclid/combine.ipynb 5
 class Combiner:
     """Combines Euclid calibrated dithers."""
 
@@ -290,13 +177,13 @@ class Combiner:
         if filt == "VIS":
             det_ids = [f"{col}-{row}" for row in range(1, 7) for col in range(1, 7)]
             detectors = [f"CCDID{i}" for i in det_ids]
-            swarp_config = swarp_config_vis
-            pix_scale = 0.1  # arcsec per pixel
+            swarp_config = SWARP_CONFIG_VIS
+            pix_scale = VIS.pix_scale  # arcsec per pixel
         else:
             det_ids = [f"{col}{row}" for row in range(1, 5) for col in range(1, 5)]
             detectors = [f"DET{i}" for i in det_ids]
-            swarp_config = swarp_config_nisp
-            pix_scale = 0.3  # arcsec per pixel
+            swarp_config = SWARP_CONFIG_NISP
+            pix_scale = NISP.pix_scale  # arcsec per pixel
         out_fn = dithers[0].name.split("-")[0] + "-STK-IMAGE" + f"_{filt}"
         if name is None and len(obs_ids) >= 1:
             name = "-".join([str(obs_id) for obs_id in obs_ids])
