IntraClusterLight/nicl_q1

commit ceb106927be8093e7db1bdf0ba5b644ef62f264b

More care with persistence errors

nbs/euclid/persistence.ipynb
@@ -322,7 +322,8 @@
     "        rms_images, np.expand_dims(minimum_idx, 0), axis=0\n",
     "    ).squeeze()\n",
     "    # estimate rms on the minimum assuming we are taking the median of n-1 values\n",
-    "    minimum_rms *= np.sqrt(np.pi / 2) / np.sqrt(n_ok - 1)\n",
+    "    n_ok_ok = n_ok > 1\n",
+    "    minimum_rms[n_ok_ok] *= np.sqrt(np.pi / 2) / np.sqrt(n_ok[n_ok_ok] - 1)\n",
     "    # estimate error on the minimum from difference versus next smallest value\n",
     "    minimum_err = (images_sorted[take + 1] - minimum) / np.sqrt(2)\n",
     "    if correct:\n",
@@ -335,13 +336,14 @@
     "            bias = np.mean(m)\n",
     "            with_n = n_ok == n\n",
     "            minimum[with_n] -= bias * rms\n",
-    "    # now set invalid pixels to zero\n",
+    "    # subtract the median background\n",
+    "    min_med = np.median(minimum[~invalid])\n",
+    "    minimum -= min_med\n",
+    "    # set invalid pixels to zero\n",
     "    invalid = np.isnan(minimum)\n",
     "    minimum[invalid] = 0\n",
-    "    # subtract the median background\n",
-    "    min_med = np.median(minimum)\n",
-    "    minimum -= min_med\n",
-    "    minimum[invalid] = 0\n",
+    "    minimum_rms[invalid] = 0\n",
+    "    minimum_err[invalid] = 0\n",
     "    return minimum, minimum_err, minimum_rms, minimum_idx"
    ]
   },

nicl/euclid/persistence.py
@@ -105,7 +105,8 @@ def minimum_map(
         rms_images, np.expand_dims(minimum_idx, 0), axis=0
     ).squeeze()
     # estimate rms on the minimum assuming we are taking the median of n-1 values
-    minimum_rms *= np.sqrt(np.pi / 2) / np.sqrt(n_ok - 1)
+    n_ok_ok = n_ok > 1
+    minimum_rms[n_ok_ok] *= np.sqrt(np.pi / 2) / np.sqrt(n_ok[n_ok_ok] - 1)
     # estimate error on the minimum from difference versus next smallest value
     minimum_err = (images_sorted[take + 1] - minimum) / np.sqrt(2)
     if correct:
@@ -118,13 +119,14 @@ def minimum_map(
             bias = np.mean(m)
             with_n = n_ok == n
             minimum[with_n] -= bias * rms
-    # now set invalid pixels to zero
+    # subtract the median background
+    min_med = np.median(minimum[~invalid])
+    minimum -= min_med
+    # set invalid pixels to zero
     invalid = np.isnan(minimum)
     minimum[invalid] = 0
-    # subtract the median background
-    min_med = np.median(minimum)
-    minimum -= min_med
-    minimum[invalid] = 0
+    minimum_rms[invalid] = 0
+    minimum_err[invalid] = 0
     return minimum, minimum_err, minimum_rms, minimum_idx
 
 # %% ../../nbs/euclid/persistence.ipynb 11

commit 318b97e11fe6a6a3470e6646939814d2acbde790

Fix bug introduced in ceb10692

nbs/euclid/persistence.ipynb
@@ -337,10 +337,10 @@
     "            with_n = n_ok == n\n",
     "            minimum[with_n] -= bias * rms\n",
     "    # subtract the median background\n",
+    "    invalid = np.isnan(minimum)\n",
     "    min_med = np.median(minimum[~invalid])\n",
     "    minimum -= min_med\n",
     "    # set invalid pixels to zero\n",
-    "    invalid = np.isnan(minimum)\n",
     "    minimum[invalid] = 0\n",
     "    minimum_rms[invalid] = 0\n",
     "    minimum_err[invalid] = 0\n",

nicl/euclid/persistence.py
@@ -120,10 +120,10 @@ def minimum_map(
             with_n = n_ok == n
             minimum[with_n] -= bias * rms
     # subtract the median background
+    invalid = np.isnan(minimum)
     min_med = np.median(minimum[~invalid])
     minimum -= min_med
     # set invalid pixels to zero
-    invalid = np.isnan(minimum)
     minimum[invalid] = 0
     minimum_rms[invalid] = 0
     minimum_err[invalid] = 0