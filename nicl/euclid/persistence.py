IntraClusterLight/nicl_q1

commit 9a07772748bba5d988a5a58d16c84d8a4de5344e

Mask image before estimating banding correction

nbs/euclid/persistence.ipynb
@@ -138,6 +138,7 @@
     "    get_obs_id_from_filename,\n",
     "    get_primary_header,\n",
     "    get_persistence_mask,\n",
+    "    get_invalid_mask,\n",
     "    get_invalid_mask_without_persistence,\n",
     "    get_rms,\n",
     "    fits_append,\n",
@@ -294,7 +295,9 @@
     "        if skyflat_path is not None:\n",
     "            img = read_and_apply_skyflat(img, fn, extname, skyflat_path)\n",
     "        if correct_banding:\n",
-    "            correction = banding_correction(img)\n",
+    "            invalid = get_invalid_mask(fn, extname)\n",
+    "            masked_image = img.where(~invalid)\n",
+    "            correction = banding_correction(masked_image)\n",
     "            img = img - correction\n",
     "        images.append(img)\n",
     "    images = np.array(images)\n",

nicl/euclid/persistence.py
@@ -31,6 +31,7 @@ from nicl.euclid.utilities import (
     get_obs_id_from_filename,
     get_primary_header,
     get_persistence_mask,
+    get_invalid_mask,
     get_invalid_mask_without_persistence,
     get_rms,
     fits_append,
@@ -77,7 +78,9 @@ def minimum_map(
         if skyflat_path is not None:
             img = read_and_apply_skyflat(img, fn, extname, skyflat_path)
         if correct_banding:
-            correction = banding_correction(img)
+            invalid = get_invalid_mask(fn, extname)
+            masked_image = img.where(~invalid)
+            correction = banding_correction(masked_image)
             img = img - correction
         images.append(img)
     images = np.array(images)

commit cc6656b7bafcf28193d9be2de43f7d85c14d2bdc

Fix error in persistence introduced in 9a07772

nbs/euclid/persistence.ipynb
@@ -296,7 +296,7 @@
     "            img = read_and_apply_skyflat(img, fn, extname, skyflat_path)\n",
     "        if correct_banding:\n",
     "            invalid = get_invalid_mask(fn, extname)\n",
-    "            masked_image = img.where(~invalid)\n",
+    "            masked_image = np.where(invalid, np.nan, img)\n",
     "            correction = banding_correction(masked_image)\n",
     "            img = img - correction\n",
     "        images.append(img)\n",

nicl/euclid/persistence.py
@@ -79,7 +79,7 @@ def minimum_map(
             img = read_and_apply_skyflat(img, fn, extname, skyflat_path)
         if correct_banding:
             invalid = get_invalid_mask(fn, extname)
-            masked_image = img.where(~invalid)
+            masked_image = np.where(invalid, np.nan, img)
             correction = banding_correction(masked_image)
             img = img - correction
         images.append(img)