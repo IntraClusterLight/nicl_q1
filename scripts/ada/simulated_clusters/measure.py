29888e9415628080d3fbeb7c088b85ede094a403
Steven Bamford <steven@stevenbamford.com>
2025-11-01 10:49:36 +0000

    Fix simulated_clusters measure script

--- scripts/ada/simulated_clusters/measure.py
+++ scripts/ada/simulated_clusters/measure.py
@@ -20,15 +20,16 @@ from nicl.euclid.utilities import default_data_path
 if __name__ == "__main__":
     configure_logging(level="DEBUG")
     configure_logging(name="nicl.mask", level="INFO")
+    data_path = default_data_path("Q1_R1_simulated_clusters")
     table = Table.read(
         default_data_path("catalogues/Hydrangea_Mocks_Sample.csv"), format="csv"
     )
     # There are 819 rows in this table, currently just testing on first two rows
-    task_id = int(os.getenv["SLURM_ARRAY_TASK_ID"])
+    task_id = int(os.getenv("SLURM_ARRAY_TASK_ID"))
     row = table[task_id - 1]
     for subfolder in ["with_subhaloes", "without_subhaloes"]:
-        image_dir = f"Q1_R1_simulated_clusters/{subfolder}/images"
-        output_dir = f"Q1_R1_simulated_clusters/{subfolder}/measurements/"
+        image_dir = data_path / f"{subfolder}/images"
+        output_dir = data_path / f"{subfolder}/measurements/"
         ra = row["RA"]
         dec = row["Dec"]
         centre = SkyCoord(ra=ra, dec=dec, unit="deg")
